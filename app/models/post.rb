21a1476c866b06372220ea3c83d29225b328acf5
Paul Famiglietti <p0seid0n_@hotmail.com>
2015-05-24 15:48:14 -0700

    Photo upload and AWS

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,17 @@
 class Post < ActiveRecord::Base
   validates :photo,  presence: true
-  default_scope -> { order(created_at: :desc) }  
+  default_scope -> { order(created_at: :desc) } 
+  mount_uploader :photo, PhotoUploader 
+  validate  :photo_size
+  
+  
+  private
+
+    # Validates the size of an uploaded picture.
+    def photo_size
+      if photo.size > 5.megabytes
+        errors.add(:photo, "should be less than 5MB")
+      end
+    end
+
 end
